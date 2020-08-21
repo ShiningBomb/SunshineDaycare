89493181506b5281d921f7378c9dfdac45e61c36
Irwan Gunardi <irwan_gunardi@hotmail.com>
2020-08-21 20:40:59 +0700

    Schedule

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'weekly_schedules/index'
+  get 'weekly_schedules/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
