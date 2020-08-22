09a28245d0ecc98d88410f9db3f551a4f69034a5
Irwan Gunardi <irwan_gunardi@hotmail.com>
2020-08-23 02:57:41 +0700

    .

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,8 +1,8 @@
 class Activity < ApplicationRecord
   belongs_to :daily_schedule
-  validates :name, :start_time, :end_time, :presence => true
+  validates :name, :start_time, :end_time, :location, :category, :presence => true
   validates :end_time, after_date: :start_time
 
-  enum location: ['Room A', 'Room B', 'Room C', 'Library', 'Playground', 'Garden', 'West Garden']
-  enum category: ['Meal', 'Cooking', 'Outdoor', 'Reading', 'English', 'Lecture']
+  enum location: ['Room A', 'Room B', 'Room C', 'Library', 'Playground', 'Garden', 'West Garden', 'Outbound']
+  enum category: ['Meal', 'Cooking', 'Social', 'Outdoor', 'Reading', 'English', 'Lecture']
 end
--- app/models/program.rb
+++ app/models/program.rb
@@ -1,3 +1,4 @@
 class Program < ApplicationRecord
     has_many :weekly_schedules
+    validates :name, :from_age, :to_age, :presence => true
 end
