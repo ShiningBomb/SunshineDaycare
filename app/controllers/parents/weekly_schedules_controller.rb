88ac0b885def6a21c7b5ac95de9ece039132d780
Irwan Gunardi <irwan_gunardi@hotmail.com>
2020-08-23 23:35:42 +0700

    BUGS

--- app/controllers/parents/weekly_schedules_controller.rb
+++ app/controllers/parents/weekly_schedules_controller.rb
@@ -2,6 +2,6 @@ class Parents::WeeklySchedulesController < ParentsController
   def index
     @child = current_parent.children.find(params[:child_id])
     @program = @child.program
-    @weekly_schedules = @program.weekly_schedules
+    @weekly_schedules = @program.weekly_schedules.order(:start_date)
   end
 end
