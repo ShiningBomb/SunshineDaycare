dc866937855253d7422ed85c0457d435662cead6
Irwan Gunardi <irwan_gunardi@hotmail.com>
2020-08-23 22:01:05 +0700

    fIX

--- app/models/weekly_schedule.rb
+++ app/models/weekly_schedule.rb
@@ -1,10 +1,8 @@
 class WeeklySchedule < ApplicationRecord
   belongs_to :program
   has_many :daily_schedules
-  
-  validates :start_date, :presence => true
 
-  before_validation :init_start_date
+  before_create :init_start_date
   after_create :init_daily_schedules
   
   def publish
@@ -15,7 +13,7 @@ class WeeklySchedule < ApplicationRecord
 
   def init_start_date
     self.start_date = Date.today.beginning_of_week
-    last_week = WeeklySchedule.order("start_date").last
+    last_week = program.weekly_schedules.order("start_date DESC").first
     
     if last_week
       self.start_date = last_week.start_date + 7.days
